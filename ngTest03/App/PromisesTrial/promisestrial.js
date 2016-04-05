c63cf7b00498d4af5ee01035306f5235262a1b87
Nick Papatonis <npapatonis@guard1.com>
2016-04-05 18:36:14 -0400

    Changed promises trial back to simple case.

--- ngTest03/App/PromisesTrial/promisestrial.js
+++ ngTest03/App/PromisesTrial/promisestrial.js
@@ -18,36 +18,36 @@
                 return deferred.promise;
             };
 
-            //var twoAsync = function () {
-            //    return oneAsync().then(function (result) {
-            //        console.log('twoAsync -- ' + result);
-            //        return result;
-            //    },
-            //    function (error) {
-            //        console.log('twoAsync -- ' + error);
-            //        return error;
-            //    })
-            //};
-
             var twoAsync = function () {
                 return oneAsync().then(function (result) {
-                    var deferred = $q.defer();
-                    console.log('twoAsync.success start -- ' + result);
-
-                    $timeout(function () {
-                        console.log('twoAsync.success finished');
-                        deferred.resolve('twoAsync.success resolved');
-                    }, 2000);
-
-                    console.log('twoAsync.success return');
-                    //return deferred.promise;
+                    console.log('twoAsync -- ' + result);
                     return result;
                 },
                 function (error) {
-                    console.log('twoAsync.error -- ' + error);
+                    console.log('twoAsync -- ' + error);
                     return error;
                 })
             };
+
+            //var twoAsync = function () {
+            //    return oneAsync().then(function (result) {
+            //        var deferred = $q.defer();
+            //        console.log('twoAsync.success start -- ' + result);
+
+            //        $timeout(function () {
+            //            console.log('twoAsync.success finished');
+            //            deferred.resolve('twoAsync.success resolved');
+            //        }, 2000);
+
+            //        console.log('twoAsync.success return');
+            //        //return deferred.promise;
+            //        return result;
+            //    },
+            //    function (error) {
+            //        console.log('twoAsync.error -- ' + error);
+            //        return error;
+            //    })
+            //};
 
             $scope.start = function () {
                 twoAsync().then(function (result) {
