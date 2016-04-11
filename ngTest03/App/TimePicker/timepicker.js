ba100ebe96fc7605e621df50b041350db333b32c
Nick Papatonis <npapatonis@guard1.com>
2016-04-11 07:47:43 -0400

    Working on manual triggering.

--- ngTest03/App/TimePicker/timepicker.js
+++ ngTest03/App/TimePicker/timepicker.js
@@ -2,10 +2,19 @@
     'use strict';
 
     angular.module('ui.bootstrap.demo').controller('TimepickerDemoCtrl', function ($scope) {
+
+        //$scope.fromDatePopup = {
+        //    isOpen: false,
+        //    Open: function () {
+        //        $scope.fromDatePopup.isOpen = true;
+        //    }
+        //};
+
         $scope.time = new Date(1970, 0, 1, 10, 30, 40);
         $scope.selectedTimeAsNumber = 10 * 36e5 + 30 * 6e4 + 40 * 1e3;
         $scope.selectedTimeAsString = '10:00';
         $scope.sharedDate = new Date(new Date().setMinutes(0, 0));
+
     });
 
 })();
