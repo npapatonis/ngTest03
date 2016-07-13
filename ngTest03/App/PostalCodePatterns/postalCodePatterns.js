d4f5a0208282f802b8dababf4450bef5eafac044
Nick Papatonis <npapatonis@guard1.com>
2016-07-12 21:32:00 -0400

    Added row class div.

--- ngTest03/App/PostalCodePatterns/postalCodePatterns.js
+++ ngTest03/App/PostalCodePatterns/postalCodePatterns.js
@@ -8,6 +8,9 @@
             { country: 'Canada', pattern: '' },
             { country: 'Mexico', pattern: '' }
         ];
+        $scope.formData = {
+            selectedPostalCode : $scope.postalCodes[0]
+        };
         $scope.postalCode = $scope.postalCodes[0];
     });
 
