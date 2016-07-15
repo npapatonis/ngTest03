4f1dd64e460c133acf0687347b1c12f900618eda
Nick Papatonis <npapatonis@guard1.com>
2016-07-15 17:32:51 -0400

    Implemented variable templates.

--- ngTest03/App/Directives/tksAddressEntry.js
+++ ngTest03/App/Directives/tksAddressEntry.js
@@ -11,7 +11,12 @@
                 address: '=',  // Bind isolate scope's address property to attribute of the same name
                 change: '&'
             },
-            templateUrl: './app/Templates/tksAddressEntry.tpl.html',
+            templateUrl: function (elem, attrs) {
+                if (attrs.templateUrl) {
+                    return attrs.templateUrl;
+                }
+                return './app/Templates/tksAddressEntry.tpl.html';
+            },
             controller: controller
         }
 
@@ -19,33 +24,35 @@
 
         function controller($scope, addressFactory) {
 
-            $scope.country = null;
+            $scope.countryChange = function () {
+                $scope.address.country = $scope.country.name;
+                $scope.useStateSelector = ($scope.country.isoCode === 'US');
+                $scope.address.state = '';
+                $scope.change();
 
-            $scope.useStateSelector = false;
+                //if ($scope.country) {
+                //    $scope.address.country = $scope.country.name;
+                //    $scope.useStateSelector = ($scope.country.isoCode === 'US');
+                //}
+                //else {
+                //    $scope.address.country = '';
+                //    $scope.useStateSelector = false;
+                //}
+                //$scope.address.state = '';
+
+                //$scope.change();
+            }
 
             init();
 
             function init() {
                 $scope.usStates = addressFactory.getUsStates();
                 $scope.countries = addressFactory.getCountries();
+
+                $scope.useStateSelector = false;
+
                 $scope.country = $scope.countries[0];
-
-                //$scope.address.country = $scope.country.name;
-                //$scope.useStateSelector = ($scope.country.isoCode === 'US');
-            }
-
-            $scope.countryChange = function () {
-                if ($scope.country) {
-                    $scope.address.country = $scope.country.name;
-                    $scope.useStateSelector = ($scope.country.isoCode === 'US');
-                }
-                else {
-                    $scope.address.country = '';
-                    $scope.useStateSelector = false;
-                }
-                $scope.address.state = '';
-
-                $scope.change();
+                $scope.countryChange();
             }
         }
 
